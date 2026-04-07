910adc54fd590671e5b32cba6ba25a1b95fdc416
ayaxxchanz <aliya.genius@gmail.com>
2026-04-07 18:01:58 +0800

    01-counter-app

---
+++ 01-counter-app/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0);
+
+// or can use function but need to use .value
+const decrement = () => {
+  counter.value--;
+}
+
+</script>
+
+<template>
+  <h1>Counter: {{ counter }}</h1>
+
+  <button @click="counter++">Increment +1</button>
+  <button @click="counter--">Decrement -1</button>
+</template>
+
+<style scoped></style>
