ae0c2a4cfdc95ad56e1bffbb46dbc9322c390447
ayaxxchanz <aliya.genius@gmail.com>
2026-04-13 18:14:59 +0800

    fix cors

--- 03-course-hub/server/server.js
+++ 03-course-hub/server/server.js
@@ -6,6 +6,16 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+
 server.use(middlewares)
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
