2eac2152d357971dd12099cdc17a219eabf5be24
ayaxxchanz <aliya.genius@gmail.com>
2026-04-13 17:38:58 +0800

    add json-server backend

---
+++ 03-course-hub/server/server.js
@@ -0,0 +1,13 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running')
+})
